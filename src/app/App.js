5ea78a4fb69df00018632f7acec0a51b728092ab
kamalasaurus <kamalasaurus@gmail.com>
2018-09-24 19:54:23 -0400

    creating some DOM

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,34 @@
 import m from '../../node_modules/mithril/mithril.js';
 import Canvas from './graphics/Canvas.js';
 
+// control buttons //////////////////////////////////////////////////////
+import BrushSelect from './graphics/controls/BrushSelect.js';
+import ColorWheel from './graphics/controls/ColorWheel.js';
+import LineWeight from './graphics/controls/LineWeight.js';
+import Undo from './graphics/controls/Undo.js';
+import Redo from './graphics/controls/Redo.js';
+import Save from './graphics/controls/Save.js';
+import Clear from './graphics/controls/Clear.js';
+// end control buttonss /////////////////////////////////////////////////
+
 export default function App(root) {
-  // initialize weassembly either here or in a double fancy canvas module
+  // initialize webassembly either here or in a double fancy canvas module
   m.route(root, '/', {
     '/': new Canvas()
+  });
+  m.route(root, '/', {
+    '/': m('div.container', [
+      (new Canvas()),
+      m('div.controls', [
+        (new BrushSelect()),
+        (new ColorWheel()),
+        (new LineWeight()),
+        (new Undo()),
+        (new Redo()),
+        (new Save()),
+        (new Clear())
+      ])
+    ])
   });
 };
 
