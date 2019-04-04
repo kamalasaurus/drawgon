530ef7ec600d8e578bf1502f9f0e79909d093b93
kamalasaurus <kamalasaurus@gmail.com>
2019-04-04 01:17:06 -0400

    added dependencies intermediary

---
+++ dependencies.js
@@ -0,0 +1,5 @@
+import { h, render, Component } from './node_modules/preact/dist/preact.mjs';
+import { Router, route } from './node_modules/preact-router/dist/preact-router.es.js';
+
+export { Router, h, render, Component };
+
