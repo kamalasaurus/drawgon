ac3bcdd6bf7679b9b2ba1f1599c30a236f346fea
kamalasaurus <kamalasaurus@gmail.com>
2019-04-23 00:07:14 -0400

    nominal triangle brush

--- src/app/options/Brushes.js
+++ src/app/options/Brushes.js
@@ -19,7 +19,15 @@ export default {
     draw(false, 'stroke');
   },
   triangleVector(draw, {pX, pY, x, y, force, lineWidth}) {
-    console.log(this.name);
+    const width = (5*(1 - force) + 0.1) * (x - pX);
+    const height = (5*(1 - force) + 0.1) * (y - pY);
+    draw(false, 'beginPath');
+    draw(false, 'moveTo', pX, pY);
+    draw(false, 'lineTo', pX + width, pY);
+    draw(false, 'lineTo', (pX + (width / 2)), pY + height);
+    draw(false, 'closePath');
+    draw(true, 'lineWidth', lineWidth);
+    draw(false, 'stroke');
   }
 };
 
