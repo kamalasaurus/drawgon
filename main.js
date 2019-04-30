9181ba83a0c58bb87b4550d31155a1c6fe55df8b
kamalasaurus <kamalasaurus@gmail.com>
2019-04-30 15:40:04 -0400

    print debugging

--- main.js
+++ main.js
@@ -2,8 +2,12 @@ import App from './src/app/App.js';
 
 void function() {
 
-  if ('serviceWorker' in navigator)
-    navigator.serviceWorker.register('service-worker.js', { scope: '/webdraw/' });
+  if ('serviceWorker' in navigator) {
+    if (/webdraw/.test(window.location.href))
+      navigator.serviceWorker.register('service-worker.js', { scope: '/webdraw/' });
+    else
+      navigator.serviceWorker.register('service-worker.js', { scope: '/' });
+  }
 
   new App(document.getElementById('app'));
 
--- service-worker.js
+++ service-worker.js
@@ -68,6 +68,7 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
+  console.log(event.request);
 	event.respondWith(
 		caches
       .match(event.request)
