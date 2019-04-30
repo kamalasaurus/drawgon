896c76a7a192b970b673af08715fa71828349227
kamalasaurus <kamalasaurus@gmail.com>
2019-04-30 15:34:37 -0400

    updated service-worker, still have problems

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
-const PRECACHE_URLS = [
+let cacheName = 'v1';
+
+let cacheFiles = [
   "android-chrome-192x192.png",
   "android-chrome-512x512.png",
   "apple-touch-icon.png",
@@ -14,68 +16,83 @@ const PRECACHE_URLS = [
   "safari-pinned-tab.svg",
   "service-worker.js",
   "src/app/App.js",
+  "src/app/Controller.js",
+  "src/app/components/Button.js",
   "src/app/components/Canvas.js",
+  "src/app/components/Container.js",
+  "src/app/components/New.js",
+  "src/app/components/controls/Brush.js",
+  "src/app/components/controls/Clear.js",
+  "src/app/components/controls/Redo.js",
+  "src/app/components/controls/Save.js",
+  "src/app/components/controls/Undo.js",
+  "src/app/components/panels/BrushPanel.js",
+  "src/app/components/panells/SavelPanel.js",
+  "src/app/components/icons/controls/Brush.js",
+  "src/app/components/icons/controls/Clear.js",
+  "src/app/components/icons/controls/Redo.js",
+  "src/app/components/icons/controls/Save.js",
+  "src/app/components/icons/controls/Undo.js",
+  "src/app/components/icons/cursors/Pencil.js",
+  "src/app/components/options/Brushes.js",
   "src/style.css",
   "node_modules/mithril/mithril.js",
   "node_modules/pressure/dist/pressure.js",
   "node_modules/downloadjs/download.js",
   "node_modules/canvas2svg/canvas2svg.js"
-]
+];
 
-const PRECACHE = 'version1';
-const RUNTIME = 'runtime';
-
-// caches our resources
-// caches is now a global object!
-// self refers to the SeviceWorkerGlobalContext -- it's a new semantic
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(PRECACHE)
-      .then(cache => cache.addAll(PRECACHE_URLS))
-      .then(self.skipWaiting())
+    caches
+      .open(cacheName)
+      .then(function(cache) {
+        return cache.addAll(cacheFiles);
+      }, function(err) {
+        console.log('service worker on install', err)
+      })
   );
 });
 
-// cleans out old caches
-self.addEventListener('activate', event => {
-  const currentCaches = [PRECACHE, RUNTIME];
+self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys()
-      .then(cacheNames => {
-        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+    caches
+      .keys()
+      .then(function(cacheNames) {
+        return Promise.all(cacheNames.map(function(thisCacheName) {
+          if (thisCacheName !== cacheName) return caches.delete(thisCacheName);
+        }))
+        .catch(function(err) { console.log('service worker on activate', err) });
       })
-      .then(cachesToDelete => {
-        return Promise.all(cachesToDelete.map(cacheToDelete => {
-          return caches.delete(cacheToDelete);
-        }));
-      })
-      .then(() => self.clients.claim())
-      //self.clients.claim will make the new service worker take effect immediately on any open pages in its scope.
   );
 });
 
-self.addEventListener('fetch', event => {
-  // skip x-origin requests
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+      .match(event.request)
+			.then(function(response) {
+				if (response) return response;
 
-  //TODO: figure out why this is broken :(
-
-  // if (event.request.url.startsWith(self.location.origin)) {
-  //   event.respondWith(
-  //     caches.match(event.request)
-  //       .then(cachedResponse => {
-  //         if (cachedResponse) return cachedResponse;
-
-  //         return caches.open(RUNTIME)
-  //           .then(cache => {
-  //             return fetch(event.request)
-  //               .then(response => {
-  //                 return cache.put(event.request, response.clone())
-  //                   .then(() => response);
-  //               });
-  //           });
-
-  //       })
-  //   );
-  // }
+				var requestClone = event.request.clone();
+				return fetch(requestClone)
+					.then(function(response) {
+				    if (!response) return response;
+					  var responseClone = response.clone();
+				    caches
+              .open(cacheName)
+              .then(function(cache) {
+							  cache.put(event.request, responseClone);
+							  return response;
+				      }, function(err) {
+                console.log('service worker on request clone', err)
+              }); // end caches.open
+          }, function(err) {
+            console.log('service worker on fetch', err);
+          })
+      }, function(err) {
+        console.log('service worker on fetch match', err);
+      })
+	);
 });
 
