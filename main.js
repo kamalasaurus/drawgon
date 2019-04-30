7a35a246a37285ddc61aff417fb63596c3ebaf67
kamalasaurus <kamalasaurus@gmail.com>
2019-04-30 15:35:45 -0400

    messing w/ service worker

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import App from './src/app/App.js';
 void function() {
 
   if ('serviceWorker' in navigator)
-    navigator.serviceWorker.register('service-worker.js', { scope: '/' });
+    navigator.serviceWorker.register('service-worker.js', { scope: '/webdraw/' });
 
   new App(document.getElementById('app'));
 
