023d01ebf17f7bc8bf39b9df36e9806639f0c927
kamalasaurus <kamalasaurus@gmail.com>
2019-05-12 21:25:03 +0200

    some refactor stuff

--- src/app/App.js
+++ src/app/App.js
@@ -4,16 +4,17 @@
 import m from '../../node_modules/mithril/mithril.js';
 
 import Controller from './Controller.js';
+import Page from './components/Page.js';
 import Container from './components/Container.js';
 import New from './components/New.js';
 import Canvas from './components/Canvas.js';
 
 import Brush from './components/controls/Brush.js';
-import Undo from './components/controls/Undo.js';
-import Redo from './components/controls/Redo.js';
+//import Undo from './components/controls/Undo.js';
+//import Redo from './components/controls/Redo.js';
 
-import Save from './components/controls/Save.js';
-import Clear from './components/controls/Clear.js';
+//import Save from './components/controls/Save.js';
+//import Clear from './components/controls/Clear.js';
 
 import BrushPanel from './components/panels/BrushPanel.js';
 import SavePanel from './components/panels/SavePanel.js';
@@ -60,22 +61,22 @@ export default function App(root) {
   //});
 
   m.route(root, '/', {
-    '/': Container('layout', [
+    '/': Page([
       m(New, {ctrl})
     ]),
-    '/draw': Container('layout', [
+    '/draw': Page([
       //m(new Canvas(ctrl)),
-      m(Container, { name: 'controls' }, [
-          m(Brush, [
+      m(Container, { name: 'controls' , children: [
+          m(Brush, { children: [
             m(BrushPanel, {ctrl})
-          ]),
+          ]}),
           //m(new Undo(ctrl)),
           //m(new Redo(ctrl)),
           //m(new Save(ctrl, [
             //m(SavePanel, {ctrl})
           //])),
           //m(new Clear(ctrl))
-      ])
+      ]})
     ])
   });
 
