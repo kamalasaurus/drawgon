86f3630ca9a4e391ae24495bb1b9bb52c8e7b9f9
kamalasaurus <kamalasaurus@gmail.com>
2019-05-12 22:31:00 +0200

    deactivated service worker

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ import App from './src/app/App.js';
 
 void function() {
 
-  if ('serviceWorker' in navigator) {
-    if (/drawgon/.test(window.location.href))
-      navigator.serviceWorker.register('./service-worker.js', { scope: '/drawgon/' });
-    else
-      navigator.serviceWorker.register('./service-worker.js', { scope: '/' });
-  }
+  //if ('serviceWorker' in navigator) {
+    //if (/drawgon/.test(window.location.href))
+      //navigator.serviceWorker.register('./service-worker.js', { scope: '/drawgon/' });
+    //else
+      //navigator.serviceWorker.register('./service-worker.js', { scope: '/' });
+  //}
 
   new App(document.getElementById('app'));
 
